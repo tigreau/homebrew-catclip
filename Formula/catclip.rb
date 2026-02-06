7652dbe3b503c9b9af36c402e5580c7f8c901ba2
Chris <56563947+Jayce132@users.noreply.github.com>
2026-02-06 13:43:12 +0200

    Bump catclip to v0.1.2

--- Formula/catclip.rb
+++ Formula/catclip.rb
@@ -1,18 +1,20 @@
 class Catclip < Formula
   desc "Copy project files to clipboard with safe ignores"
   homepage "https://github.com/tigreau/catclip"
-  url "https://github.com/tigreau/catclip/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "c06b07d5f3014e8fbca31bc2ea2542ee761a47092889ae122df1a62af9a8bf5d"
+  url "https://github.com/tigreau/catclip/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "00c08960311dc9023eade9922f59eb87451be541bff9ecfeeb106973de684f0a"
   license "MIT"
 
   def install
     bin.install "catclip"
-    bin.install "VERSION"
-    pkgshare.install "ignore.yaml"
+    pkgshare.install "ignore.yaml", "VERSION"
   end
 
   test do
     ENV["XDG_CONFIG_HOME"] = testpath/"config"
+    (testpath/"config/catclip").mkpath
+    cp pkgshare/"ignore.yaml", testpath/"config/catclip/ignore.yaml"
+
     (testpath/"sample.txt").write("hi")
     output = shell_output("#{bin}/catclip --print sample.txt")
     assert_match "# File: sample.txt", output
