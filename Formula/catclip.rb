4f24e9adbb59547facd56389bfe30b93acf0232c
Chris <56563947+Jayce132@users.noreply.github.com>
2026-02-07 03:37:19 +0200

    fix: update catclip formula checksum for v0.1.3

--- Formula/catclip.rb
+++ Formula/catclip.rb
@@ -2,7 +2,7 @@ class Catclip < Formula
   desc "Copy project files to clipboard with safe ignores"
   homepage "https://github.com/tigreau/catclip"
   url "https://github.com/tigreau/catclip/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "00c08960311dc9023eade9922f59eb87451be541bff9ecfeeb106973de684f0a"
+  sha256 "dea438dfb6096ddee1f663b252b94c7ad8438b0d0b0640f70dfab5f16de44a47"
   license "MIT"
 
   def install
