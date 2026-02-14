55154b1e459a240cfddd21da443e8594ef15e235
Chris Joyce <56563947+tigreau@users.noreply.github.com>
2026-02-14 06:15:07 +0200

    feat: bump catclip to v0.1.4

--- Formula/catclip.rb
+++ Formula/catclip.rb
@@ -1,8 +1,8 @@
 class Catclip < Formula
   desc "Copy project files to clipboard with safe ignores"
   homepage "https://github.com/tigreau/catclip"
-  url "https://github.com/tigreau/catclip/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "dea438dfb6096ddee1f663b252b94c7ad8438b0d0b0640f70dfab5f16de44a47"
+  url "https://github.com/tigreau/catclip/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "f2ddbd28f967375f211b8bb5f3d0409bba737eac0d597b97bb67baa3cfd5d804"
   license "MIT"
 
   def install
