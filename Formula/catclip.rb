0adcb709bece7e038fe0541cea771c1230876564
Chris Joyce <56563947+tigreau@users.noreply.github.com>
2026-04-06 20:27:27 +0300

    feat: bump catclip to v0.3.3

--- Formula/catclip.rb
+++ Formula/catclip.rb
@@ -1,20 +1,24 @@
 class Catclip < Formula
   desc "Copy project files to clipboard with safe ignores"
   homepage "https://github.com/tigreau/catclip"
-  url "https://github.com/tigreau/catclip/archive/refs/tags/v0.3.2.tar.gz"
-  sha256 "b8250873760e0af4cc553d406a5ed23cea16a8558e039d49f18a7bf189df20de"
+  url "https://github.com/tigreau/catclip/archive/refs/tags/v0.3.3.tar.gz"
+  sha256 "9a8b11cf81a55d5870ca9674854518ba567d2e5d46348d80dd9cce9640014b94"
   license "MIT"
 
+  depends_on "go" => :build
   depends_on "fzf"
   depends_on "ripgrep"
-  depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args(output: libexec/"catclip"), "./cmd/catclip"
+    system "go", "build", *std_go_args(output: libexec/"catclip-tree"), "./cmd/catclip-tree"
+
     pkgshare.install "VERSION"
+
     (bin/"catclip").write_env_script libexec/"catclip",
-                                     CATCLIP_FZF: Formula["fzf"].opt_bin/"fzf",
-                                     CATCLIP_RG:  Formula["ripgrep"].opt_bin/"rg"
+                                     CATCLIP_FZF:  Formula["fzf"].opt_bin/"fzf",
+                                     CATCLIP_RG:   Formula["ripgrep"].opt_bin/"rg",
+                                     CATCLIP_TREE: libexec/"catclip-tree"
   end
 
   test do
